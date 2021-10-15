2674a8609d17fc1bc21d26b48977d839928fa1d3
Prathibha R <prathi2kgrs@gmail.com>
2021-10-15 13:41:01 +0530

    Adding @CrossOrigin to enable SpringBoot to listen to other application(React JS)

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.demo.model.Blog;
 import com.example.demo.service.IBlogService;
 
 @RestController
+@CrossOrigin
 public class BlogController {
 	
 	@Autowired
